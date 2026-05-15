462ff0e972d3fb74f200f391993103932939b10c
madnan-digi <digigit!@#123>
2026-05-16 01:51:52 +0500

    faq section updated.

--- resources/views/frontend/components/faqs.blade.php
+++ resources/views/frontend/components/faqs.blade.php
@@ -14,7 +14,7 @@
                 <div id="accordion-card" data-accordion="collapse">
                     {{--===========--}}
                     <h2 id="faq-1">
-                        <button type="button" class="flex items-center justify-between w-full p-4 font-medium text-heading
+                        <button type="button" class="flex items-center justify-between w-full p-4 font-medium text-white text-heading
                         rounded-lg bg-gradient-to-r from-mst to-mst-dark text-md
                          gap-3 [&[aria-expanded='true']]:rounded-b-none [&[aria-expanded='true']]:shadow-none" data-accordion-target="#accordion-card-body-1" aria-expanded="true" aria-controls="accordion-card-body-1">
                             <span>What are the best tours to book in Dubai?</span>
@@ -37,16 +37,17 @@
                     </div>
                     {{--===========--}}
                     <h2 id="faq-2" class="mt-6">
-                        <button type="button" class="flex items-center justify-between w-full p-4 font-medium
-                        text-heading rounded-lg border border-gray-200 text-mst-gray
-                         gap-3 [&[aria-expanded='true']]:rounded-b-none [&[aria-expanded='true']]:shadow-none" data-accordion-target="#faq-body-2" aria-expanded="false" aria-controls="faq-body-2">
+                        <button type="button" class="flex items-center justify-between w-full p-4 font-medium rounded-lg border border-gray-200 text-mst-gray bg-white gap-3 transition
+                        [&[aria-expanded='true']]:rounded-b-none [&[aria-expanded='true']]:shadow-none
+                        [&[aria-expanded='true']]:bg-gradient-to-r [&[aria-expanded='true']]:from-mst [&[aria-expanded='true']]:to-mst-dark
+                        [&[aria-expanded='true']]:text-white [&[aria-expanded='true']]:border-transparent" data-accordion-target="#faq-body-2" aria-expanded="false" aria-controls="faq-body-2">
                             <span>How much does a Dubai desert safari cost?</span>
                             <svg data-accordion-icon class="w-5 h-5 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 15 7-7 7 7"/>
                             </svg>
                         </button>
                     </h2>
-                    <div id="faq-body-2" class="hidden rounded-b-lg border border-gray-200"
+                    <div id="faq-body-2" class="hidden rounded-b-lg bg-gradient-to-r from-mst to-mst-dark"
                          aria-labelledby="faq-2">
                         <div class="pb-4 pt-0 px-4">
                             <p class="mb-2 text-body text-xs text-white">Lorem ipsum dolor sit amet, consectetur
@@ -62,16 +63,17 @@
                     </div>
                     {{--===========--}}
                     <h2 id="faq-3" class="mt-6">
-                        <button type="button" class="flex items-center justify-between w-full p-4 font-medium
-                                    text-heading rounded-lg border border-gray-200 text-mst-gray
-                                    gap-3 [&[aria-expanded='true']]:rounded-b-none [&[aria-expanded='true']]:shadow-none" data-accordion-target="#faq-body-3" aria-expanded="false" aria-controls="faq-body-3">
+                        <button type="button" class="flex items-center justify-between w-full p-4 font-medium rounded-lg border border-gray-200 text-mst-gray bg-white gap-3 transition
+                                    [&[aria-expanded='true']]:rounded-b-none [&[aria-expanded='true']]:shadow-none
+                                    [&[aria-expanded='true']]:bg-gradient-to-r [&[aria-expanded='true']]:from-mst [&[aria-expanded='true']]:to-mst-dark
+                                    [&[aria-expanded='true']]:text-white [&[aria-expanded='true']]:border-transparent" data-accordion-target="#faq-body-3" aria-expanded="false" aria-controls="faq-body-3">
                             <span>Do you provide hotel pickup and drop-off for tours?</span>
                             <svg data-accordion-icon class="w-5 h-5 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 15 7-7 7 7"/>
                             </svg>
                         </button>
                     </h2>
-                    <div id="faq-body-3" class="hidden rounded-b-lg border border-gray-200" aria-labelledby="faq-3">
+                    <div id="faq-body-3" class="hidden rounded-b-lg bg-gradient-to-r from-mst to-mst-dark" aria-labelledby="faq-3">
                         <div class="pb-4 pt-0 px-4">
                             <p class="mb-2 text-body text-xs text-white">Lorem ipsum dolor sit amet, consectetur
                                                                          adipisicing elit. Accusantium animi assumenda
@@ -86,10 +88,11 @@
                     </div>
                     {{--===========--}}
                     <h2 id="faq-4" class="mt-6">
-                        <button type="button" class="flex items-center justify-between w-full p-4 font-medium
-                                text-heading rounded-lg border border-gray-200 text-mst-gray
-                                gap-3 [&[aria-expanded='true']]:rounded-b-none
-                                [&[aria-expanded='true']]:shadow-none"
+                        <button type="button" class="flex items-center justify-between w-full p-4 font-medium rounded-lg border border-gray-200 text-mst-gray bg-white gap-3 transition
+                                [&[aria-expanded='true']]:rounded-b-none
+                                [&[aria-expanded='true']]:shadow-none
+                                [&[aria-expanded='true']]:bg-gradient-to-r [&[aria-expanded='true']]:from-mst [&[aria-expanded='true']]:to-mst-dark
+                                [&[aria-expanded='true']]:text-white [&[aria-expanded='true']]:border-transparent"
                                 data-accordion-target="#faq-body-4" aria-expanded="false" aria-controls="faq-body-4">
                             <span>Do you offer private yacht rentals in Dubai?</span>
                             <svg data-accordion-icon class="w-5 h-5 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -97,7 +100,7 @@
                             </svg>
                         </button>
                     </h2>
-                    <div id="faq-body-4" class="hidden rounded-b-lg border border-gray-200" aria-labelledby="faq-4">
+                    <div id="faq-body-4" class="hidden rounded-b-lg bg-gradient-to-r from-mst to-mst-dark" aria-labelledby="faq-4">
                         <div class="pb-4 pt-0 px-4">
                             <p class="mb-2 text-body text-xs text-white">Lorem ipsum dolor sit amet, consectetur
                                                                          adipisicing elit. Accusantium animi assumenda
@@ -112,10 +115,11 @@
                     </div>
                     {{--===========--}}
                     <h2 id="faq-5" class="mt-6">
-                        <button type="button" class="flex items-center justify-between w-full p-4 font-medium
-                                    text-heading rounded-lg border border-gray-200 text-mst-gray
-                                    gap-3 [&[aria-expanded='true']]:rounded-b-none
-                                    [&[aria-expanded='true']]:shadow-none"
+                        <button type="button" class="flex items-center justify-between w-full p-4 font-medium rounded-lg border border-gray-200 text-mst-gray bg-white gap-3 transition
+                                    [&[aria-expanded='true']]:rounded-b-none
+                                    [&[aria-expanded='true']]:shadow-none
+                                    [&[aria-expanded='true']]:bg-gradient-to-r [&[aria-expanded='true']]:from-mst [&[aria-expanded='true']]:to-mst-dark
+                                    [&[aria-expanded='true']]:text-white [&[aria-expanded='true']]:border-transparent"
                                 data-accordion-target="#faq-body-5" aria-expanded="false" aria-controls="faq-body-5">
                             <span>Can I apply for a UAE visa through your company?</span>
                             <svg data-accordion-icon class="w-5 h-5 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -123,7 +127,7 @@
                             </svg>
                         </button>
                     </h2>
-                    <div id="faq-body-5" class="hidden rounded-b-lg border border-gray-200" aria-labelledby="faq-5">
+                    <div id="faq-body-5" class="hidden rounded-b-lg bg-gradient-to-r from-mst to-mst-dark" aria-labelledby="faq-5">
                         <div class="pb-4 pt-0 px-4">
                             <p class="mb-2 text-body text-xs text-white">Lorem ipsum dolor sit amet, consectetur
                                                                          adipisicing elit. Accusantium animi assumenda
